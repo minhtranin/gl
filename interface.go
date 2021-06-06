adfe9457f14d77d7724e329e4b0e158aac4114d3
Minh T <minh@wearebloom.com.au>
2021-06-07 00:29:20 +0700

    interface

--- interface.go
+++ interface.go
@@ -1,50 +1,50 @@
-package main 
+package main
+
 import "fmt"
 
 type Animal interface {
-    fly()
-    speak()
+	fly()
+	speak()
 }
 
 type Vehical interface {
-    run()
-    sound()
+	run()
+	sound()
 }
+
 //embed interface
 type SupperAnimal interface {
-    Animal
-    Vehical
-} 
+	Animal
+	Vehical
+}
 
 type Dog struct {
-
 }
 
-func(d Dog) speak() { /// in file struct.go.......
-    fmt.Println("go go")
-}
- 
-func(d Dog) fly() { /// in file struct.go.......
-    fmt.Println("o` o`")
+func (d Dog) speak() { /// in file struct.go.......
+	fmt.Println("go go")
 }
 
-func(d Dog) run() { /// in file struct.go.......
-    fmt.Println("o` o`")
+func (d Dog) fly() { /// in file struct.go.......
+	fmt.Println("o` o`")
 }
 
-func(d Dog) sound() { /// in file struct.go.......
-    fmt.Println("o` o`")
+func (d Dog) run() { /// in file struct.go.......
+	fmt.Println("o` o`")
 }
- 
+
+func (d Dog) sound() { /// in file struct.go.......
+	fmt.Println("o` o`")
+}
+
 func goPro(i interface{}) {
-    fmt.Println(i)
+	fmt.Println(i)
 }
 
-func main () {
-   dog := Dog {
-   }
-   dog.speak()   
-   var spAnm SupperAnimal = dog
-   spAnm.speak()
-   goPro("10")
+func main() {
+	dog := Dog{}
+	dog.speak()
+	var spAnm SupperAnimal = dog
+	spAnm.speak()
+	goPro("10")
 }
--- struct.go
+++ struct.go
@@ -1,52 +1,82 @@
 package main
+
 import (
-    "fmt"
-    )
+	"fmt"
+)
+
+type Action interface {
+	fly()
+}
 
 type Animal struct {
-    name string
-    phone int
-    call func()
-    otherd *string 
+	name   string
+	phone  int
+	call   func()
+	otherd *string
 }
 
 func (n Animal) getName() string {
-        return n.name
-    }
+	return n.name
+}
 
+func (n Animal) fly() {
+	fmt.Println(n.name)
+}
 
+func (n *Animal) returnInter() Action {
+	return Animal{
+		name:  "fly return interface",
+		phone: 123123,
+	}
+}
 func (n *Animal) setName() {
-        n.name = "Minh"
-    }
+	n.name = "Minh"
+}
 
-func main () {
-    otherd := "something else"
-    fmt.Println("I ")
-    pig := Animal{
-        name: "cute",
-        phone: 9928,
-        call: func() {
-            fmt.Println("check function")
-        },
-        otherd: &otherd,
-    }
-    fmt.Println(pig)
+func (n *Animal) mm() Animal {
+	return Animal{
+		name: "pro",
+	}
+}
 
-    var student Animal = struct {
-        name string
-        phone int
-        call func()
-        otherd *string 
-    }{
-        name: "TCM",
-        phone: 999,
-    }
-    pig.call()
-    Sname := student.getName()
-    fmt.Println(student)
-    fmt.Println(Sname)
+func main() {
+	otherd := "something else"
+	fmt.Println("I ")
+	pig := Animal{
+		name:  "cute",
+		phone: 9928,
+		call: func() {
+			fmt.Println("check function")
+		},
+		otherd: &otherd,
+	}
+	fmt.Println(pig)
 
-    // method
-    student.setName()
-    fmt.Println(student.name)
+	var student Animal = struct {
+		name   string
+		phone  int
+		call   func()
+		otherd *string
+	}{
+		name:  "TCM",
+		phone: 999,
+	}
+	pig.call()
+	Sname := student.getName()
+	fmt.Println(student)
+	fmt.Println(Sname)
+
+	// method
+	student.setName()
+	fmt.Println(student.name)
+	mmstr := student.mm()
+	fmt.Println(mmstr)
+
+	var testFly Action = Animal{
+		name: "Minh testFly",
+	}
+	testFly.fly()
+	var returnF Action = student.returnInter()
+	fmt.Println(returnF.phone)
+	fmt.Println(returnF)
 }
